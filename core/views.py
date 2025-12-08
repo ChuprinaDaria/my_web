e3b5484fb99fe81dd68f42f31c1181642ad978f0
ChuprinaDaria <chuprina.dariia@gmail.com>
2025-12-08 13:13:59 +0100

    Виправлено помилку в blog_list, додано заголовок перед About карткою та Blog до навігації

--- core/views.py
+++ core/views.py
@@ -184,9 +184,8 @@ def home(request):
         from .models import AboutCard
         about_card = AboutCard.objects.filter(is_active=True).order_by('order', '-updated_at').first()
         if about_card:
-            lang_code = language.split('-')[0] if language else 'uk'
-            about_card_title = about_card.get_title(lang_code)
-            about_card_description = about_card.get_description(lang_code)
+            about_card_title = about_card.get_title(language)
+            about_card_description = about_card.get_description(language)
     except Exception as e:
         pass
 
